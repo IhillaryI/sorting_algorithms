59c6d813ec19a2d32aa1e1d72100b1ba67900b03
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-22 14:43:50 +0000

    modified file

--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -9,12 +11,24 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/**
+ * enum boolean - defines a boolean type with values true and false
+ * @false: false
+ * @true: true
+ */
+typedef enum boolean
+{
+	false,
+	true
+} bool;
+void bubble_sort(int *array, size_t size);
 #endif
