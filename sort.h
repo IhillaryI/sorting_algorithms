5605ca522f41d1eeafa06be9802b19c7102fc2e5
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-23 23:31:45 +0000

    modified file

--- sort.h
+++ sort.h
@@ -33,4 +33,5 @@ typedef enum boolean
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
