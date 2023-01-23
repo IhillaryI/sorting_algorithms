c176966964e614017d965bf29e0f3cdd6dc95891
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-23 20:59:05 +0000

    modified file

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,19 +14,19 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
-	int *min;
+	int min_idx;
 
 	for (i = 0; i < size - 1; ++i)
 	{
-		min = &array[i];
+		min_idx = i;
 		for (j = i + 1; j < size; ++j)
-			if (array[j] < *min)
+			if (array[j] < array[min_idx])
 			{
-				min = &array[j];
+				min_idx = j;
 			}
-		if (*min < array[i])
+		if ((size_t)min_idx != i)
 		{
-			swap(min, &array[i]);
+			swap(&array[min_idx], &array[i]);
 			print_array(array, size);
 		}
 	}
